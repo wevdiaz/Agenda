2bdc737a944031dca793b1b3d75dffa0a310f917
Weverson Diazz <wevers.info@gmail.com>
2022-06-14 20:49:28 -0300

    criando mensagens de sucessos

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,18 +6,27 @@ exports.index = (req, res) => {
 }
 
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
+    try {
+        const login = new Login(req.body);
+        await login.register();
 
-    if (login.errors.length > 0) {
-        req.flash("errors", login.errors);
-        
+        if (login.errors.length > 0) {
+            req.flash("errors", login.errors);            
+            req.session.save(function(){
+            return res.redirect("back");
+            });
+
+            return;
+        }
+
+        req.flash("success", "Seu usuário foi criado com sucesso!");            
         req.session.save(function(){
-           return res.redirect("back");
+        return res.redirect("back");
         });
 
-        return;
-    }
 
-    return res.send(login.errors);
+    } catch(err) {
+        console.log(err);
+        return res.render("404");
+    }
 }
