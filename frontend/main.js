378a49d7611a2f113f67f63e68645df8579b0d64
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-17 16:50:11 -0300

    validando o frontend

--- frontend/main.js
+++ frontend/main.js
@@ -3,32 +3,13 @@ import "regenerator-runtime/runtime";
 
 import './assets/css/style.css';
 
-console.log("frontend valid - verificar!");
-
-// const inputs = document.querySelectorAll(".form-group input");
-
 
 const formRegister = document.getElementById("formRegister");
+const formLogin = document.getElementById("formLogin");
 
-formRegister.addEventListener("submit", (e) => {
-  e.preventDefault();
+// formRegister.addEventListener("submit", checkFieldsForm );
 
-  const fieldInputs = formRegister.querySelectorAll(".form-group input");
-
-  for(let field of fieldInputs) {
-    
-    if (field.value == "") {
-      const divParent = field.parentNode;
-      const message = field.name === "email" ? "Informe um email válido" : "Informe uma senha válida";
-
-      console.log(field);
-      console.log(field.value);
-      console.log(field.parentNode);
-      const span = createSpan(message);
-      divParent.appendChild(span);
-    }
-  }
-});
+// formLogin.addEventListener("submit", checkFieldsForm );
 
 function createSpan(message) {
   const classError = "text-center d-block fs-6 text-danger fst-italic".split(" ");
@@ -43,4 +24,51 @@ function createSpan(message) {
   return span;
 }
 
+function clearMessageError(form) {
+  const messagesError = form.querySelectorAll(".form-group span");
+
+  if (messagesError) {
+
+    messagesError.forEach((message)=> {
+      message.remove();     
+    });
+  }
+
+}
+
+function checkFieldsForm(e) {
+
+  clearMessageError(e.target);
+
+  const fieldInputs = e.target.querySelectorAll(".form-group input");
+
+  for(let field of fieldInputs) {
+    
+    if (field.value == "") {
+      const divParent = field.parentNode;
+      const message = field.name === "email" ? "Informe um email válido" : "Informe uma senha válida";
+      
+      const span = createSpan(message);
+      divParent.appendChild(span);
+      e.preventDefault();
+    }
+  }
+}
+
+function removeMessageUser() {
+  const messageUser = document.querySelector("boxMessage");
+
+  console.log(messageUser);
+
+  if (messageUser) {
+    setTimeout(()=> {
+      messageUser.style.display= "none";
+      messageUser.style.transition= ".5s";
+    }, 3000)
+  }
+}
+
+const formContact = document.querySelector("form");
+
+formContact.addEventListener("load", removeMessageUser);
 
