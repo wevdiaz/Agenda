616bae37d95ee4d469d310aef1209eb5417deeec
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-16 21:47:06 -0300

    criando o span de error - estilizando - checando campo no frontend

--- frontend/main.js
+++ frontend/main.js
@@ -5,3 +5,42 @@ import './assets/css/style.css';
 
 console.log("frontend valid - verificar!");
 
+// const inputs = document.querySelectorAll(".form-group input");
+
+
+const formRegister = document.getElementById("formRegister");
+
+formRegister.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const fieldInputs = formRegister.querySelectorAll(".form-group input");
+
+  for(let field of fieldInputs) {
+    
+    if (field.value == "") {
+      const divParent = field.parentNode;
+      const message = field.name === "email" ? "Informe um email válido" : "Informe uma senha válida";
+
+      console.log(field);
+      console.log(field.value);
+      console.log(field.parentNode);
+      const span = createSpan(message);
+      divParent.appendChild(span);
+    }
+  }
+});
+
+function createSpan(message) {
+  const classError = "text-center d-block fs-6 text-danger fst-italic".split(" ");
+
+  const span = document.createElement("span");
+  span.textContent = message;
+  
+  classError.forEach((classText) => {
+    span.classList.add(classText);
+  })
+
+  return span;
+}
+
+
